2c215f20d88b185021d2f30fe549fc919acb5588
ian1765 <ichigada@mail.yu.edu>
2025-02-18 14:50:51 -0500

    Completed assignment

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,4 +1,4 @@
-from aim5005.features import MinMaxScaler, StandardScaler
+from aim5005.features import MinMaxScaler, StandardScaler, LabelEncoder
 import numpy as np
 import unittest
 from unittest.case import TestCase
@@ -51,6 +51,7 @@ class TestFeatures(TestCase):
         data = [[0, 0], [0, 0], [1, 1], [1, 1]]
         expected = np.array([[-1., -1.], [-1., -1.], [1., 1.], [1., 1.]])
         scaler.fit(data)
+        result = scaler.transform(data)
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
         
     def test_standard_scaler_single_value(self):
@@ -62,6 +63,46 @@ class TestFeatures(TestCase):
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
 
     # TODO: Add a test of your own below this line
-    
+    def test_standard_scaler_fit(self):
+        scaler = StandardScaler()
+        data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
+        expected_mean = np.array([-0.125,  9.   ])
+        expected_std = np.array([0.73950997, 5.91607978])
+        scaler.fit(data)
+        assert np.allclose(expected_mean, scaler.mean), "scaler fit does not return mean value [-0.125,  9.   ] "
+        assert np.allclose(expected_std, scaler.std), "scaler fit does not return standard deviation value [0.73950997, 5.91607978] "
+
+    def test_label_encoder_fit(self):
+        encoder = LabelEncoder()
+        labels = np.array(["paris", "paris", "tokyo", "amsterdam"])
+        expected_classes_ = np.array(["paris", "tokyo", "amsterdam"])
+        encoder.fit(labels)
+        assert np.array_equal(encoder.classes_, np.sort(expected_classes_))
+
+    def test_label_encoder_transform(self):
+        encoder = LabelEncoder()
+        labels = np.array(["paris", "paris", "tokyo", "amsterdam"])
+        encoder.fit(labels)
+        expected = np.array([2, 2, 1])
+        result = encoder.transform(np.array(["tokyo", "tokyo", "paris"]))
+        assert np.array_equal(expected, result)
+
+    def test_label_encoder_transform_with_unseen_labels(self):
+        encoder = LabelEncoder()
+        labels = np.array(["paris", "paris", "tokyo", "amsterdam"])
+        encoder.fit(labels)
+
+        with self.assertRaises(ValueError) as context:
+            encoder.transform(np.array(["tokyo", "tokyo", "london"]))
+
+        self.assertEqual(str(context.exception), "Transform called with unseen labels")
+
+    def test_label_encoder_fit_transform(self):
+        encoder = LabelEncoder()
+        labels = np.array(["tokyo", "tokyo", "paris"])
+        expected = np.array([1, 1, 0])
+        result = encoder.fit_transform(labels)
+        assert np.array_equal(expected, result)
+ 
 if __name__ == '__main__':
     unittest.main()
